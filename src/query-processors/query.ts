7356cc1270882c1c112452aa59669b033db57d6f
Leonid Fenko <fenok2112@gmail.com>
2021-06-22 16:20:18 +0300

    Make query processor configurable

--- src/query-processors/query.ts
+++ src/query-processors/query.ts
@@ -1,4 +1,4 @@
-import { StringifiableRecord } from "query-string";
+import { ParseOptions, StringifiableRecord, StringifyOptions } from "query-string";
 import { QueryProcessor } from "./interface";
 import queryString from "query-string";
 
@@ -8,16 +8,21 @@ export type ToGenericQueryParams<T> = {
     [TKey in keyof T]?: ParsedQueryValue<string | boolean | number>;
 };
 
-export function query<InQuery extends StringifiableRecord = StringifiableRecord>(): QueryProcessor<
-    InQuery,
-    ToGenericQueryParams<InQuery>
-> {
+export interface QueryOptions {
+    stringify?: StringifyOptions;
+    parse?: ParseOptions;
+}
+
+export function query<InQuery extends StringifiableRecord = StringifiableRecord>({
+    stringify,
+    parse,
+}: QueryOptions = {}): QueryProcessor<InQuery, ToGenericQueryParams<InQuery>> {
     return {
         stringify(query: InQuery): string {
-            return query && Object.keys(query).length ? `?${queryString.stringify(query)}` : "";
+            return query && Object.keys(query).length ? `?${queryString.stringify(query, stringify)}` : "";
         },
         parse(query: string): ToGenericQueryParams<InQuery> {
-            return queryString.parse(query) as ToGenericQueryParams<InQuery>;
+            return queryString.parse(query, parse) as ToGenericQueryParams<InQuery>;
         },
     };
 }
