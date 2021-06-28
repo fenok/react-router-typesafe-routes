b0e9fb2967bf042315c483685319b285973c89c0
Leonid Fenko <fenok2112@gmail.com>
2021-06-28 15:29:56 +0300

    Simplify query types

--- src/query-processors/query.ts
+++ src/query-processors/query.ts
@@ -1,28 +1,31 @@
-import queryString, { ParsedQuery, ParseOptions, StringifyOptions } from "query-string";
+import queryString, { ParseOptions, StringifyOptions } from "query-string";
 import { QueryProcessor } from "./interface";
 import { applyCasters, Caster } from "../path-processors";
 
 export type QueryOptions = StringifyOptions & ParseOptions;
 
-export type QueryParamsFromCasters<T, AddUndefinedToArray extends boolean> = {
+export type QueryTypes<Options extends QueryOptions = Record<string, unknown>, T = string | number | boolean> =
+    | T
+    | null
+    | (CanStoreNullInArray<Options> extends true ? T | null : T)[];
+
+export type QueryParamsFromCasters<T, Loose extends boolean = false> = {
     [Key in keyof T]?: T[Key] extends Caster<infer Type>[] | Caster<infer Type>
         ? Type extends (infer ArrayType)[]
-            ? AddUndefined<ArrayType, AddUndefinedToArray>[]
+            ? AddUndefined<ArrayType, Loose>[]
             : Type
         : never;
 };
 
 export type AddUndefined<T, Add extends boolean> = true extends Add ? T | undefined : T;
 
-export type GetKnownTypes<T extends QueryOptions> = T extends {}
-    ? T["parseBooleans"] extends true
-        ? T["parseNumbers"] extends true
-            ? string | number | boolean | (true extends CanStoreNullInArray<T> ? null : string)
-            : string | boolean | (true extends CanStoreNullInArray<T> ? null : string)
-        : T["parseNumbers"] extends true
-        ? string | number | (true extends CanStoreNullInArray<T> ? null : string)
-        : string | (true extends CanStoreNullInArray<T> ? null : string)
-    : string | (true extends CanStoreNullInArray<T> ? null : string);
+export type KnownTypes<Options extends QueryOptions> = Options["parseBooleans"] extends true
+    ? Options["parseNumbers"] extends true
+        ? string | number | boolean
+        : string | boolean
+    : Options["parseNumbers"] extends true
+    ? string | number
+    : string;
 
 export type CanStoreNullInArray<Options extends QueryOptions> = undefined extends Options["arrayFormat"]
     ? true
@@ -30,31 +33,21 @@ export type CanStoreNullInArray<Options extends QueryOptions> = undefined extend
     ? true
     : false;
 
-export function query<Options extends QueryOptions = {}, KnownTypes = GetKnownTypes<Options>, T extends null = null>(
-    shape?: T,
+export function query<Options extends QueryOptions>(
+    shape?: null,
     options?: Options
-): QueryProcessor<Record<string, any>, ParsedQuery<KnownTypes>>;
+): QueryProcessor<Record<string, any>, Record<string, QueryTypes<Options, KnownTypes<Options>>>>;
 
 export function query<
-    Options extends QueryOptions & { parseBooleans?: false; parseNumbers?: false } = {},
-    KnownTypes = string | number | boolean | (true extends CanStoreNullInArray<Options> ? null : string),
-    T extends {
-        [Key in string]: Caster<KnownTypes | null | KnownTypes[]> | Caster<KnownTypes | null | KnownTypes[]>[];
-    } = {}
->(shape: T, options?: Options): QueryProcessor<QueryParamsFromCasters<T, true>, QueryParamsFromCasters<T, false>>;
+    Options extends QueryOptions & { parseBooleans?: false; parseNumbers?: false },
+    T extends Record<string, Caster<QueryTypes<Options>> | Caster<QueryTypes<Options>>[]>
+>(shape?: T, options?: Options): QueryProcessor<QueryParamsFromCasters<T, true>, QueryParamsFromCasters<T>>;
 
-export function query<
-    Options extends QueryOptions,
-    AllowedTypes = string | number | boolean | null,
-    T extends
-        | {
-              [Key in string]:
-                  | Caster<AllowedTypes | null | AllowedTypes[]>
-                  | Caster<AllowedTypes | null | AllowedTypes[]>[];
-          }
-        | null = null
->(shape?: T, options: Options = {} as Options): QueryProcessor<Record<string, any>, Record<string, any>> {
-    function cast(object: ParsedQuery<string | null>) {
+export function query(
+    shape?: null | Record<string, Caster<QueryTypes> | Caster<QueryTypes>[]>,
+    options: QueryOptions = {}
+): QueryProcessor<Record<string, any>, Record<string, any>> {
+    function cast(object: Record<string, any>) {
         const result: Record<string, any> = {};
 
         Object.keys(object).forEach((key) => {
