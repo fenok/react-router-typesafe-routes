da71977810543a375e6211284e217c2d5e9df511
Leonid Fenko <fenok2112@gmail.com>
2023-03-13 23:51:20 +0100

    Add deprecation warnings for assertion helpers

--- src/common/types/helpers.deprecated.ts
+++ src/common/types/helpers.deprecated.ts
@@ -1,5 +1,12 @@
+import { warn } from "../warn.js";
+
+const DEPRECATION_MESSAGE =
+    "You're using a deprecated assertion helper, either directly or via a deprecated type. Please switch to universal types.";
+
 /** @deprecated Pass a custom validator to string() instead */
 export function assertIsString(value: unknown, message?: string): asserts value is string {
+    warn(DEPRECATION_MESSAGE);
+
     if (typeof value !== "string") {
         throw new Error(message ?? `Expected ${String(value)} to be a string`);
     }
@@ -7,6 +14,8 @@ export function assertIsString(value: unknown, message?: string): asserts value 
 
 /** @deprecated Pass a custom validator to number() instead */
 export function assertIsNumber(value: unknown, message?: string): asserts value is number {
+    warn(DEPRECATION_MESSAGE);
+
     if (typeof value !== "number") {
         throw new Error(message ?? `Expected ${String(value)} to be a number`);
     }
@@ -14,6 +23,8 @@ export function assertIsNumber(value: unknown, message?: string): asserts value 
 
 /** @deprecated Pass a custom validator to boolean() instead */
 export function assertIsBoolean(value: unknown, message?: string): asserts value is boolean {
+    warn(DEPRECATION_MESSAGE);
+
     if (typeof value !== "boolean") {
         throw new Error(message ?? `Expected ${String(value)} to be a boolean`);
     }
@@ -21,6 +32,8 @@ export function assertIsBoolean(value: unknown, message?: string): asserts value
 
 /** @deprecated This shouldn't be used in universal types */
 export function assertIsArray(value: unknown, message?: string): asserts value is unknown[] {
+    warn(DEPRECATION_MESSAGE);
+
     if (!Array.isArray(value)) {
         throw new Error(message ?? `Expected ${String(value)} to be an array`);
     }
@@ -28,6 +41,8 @@ export function assertIsArray(value: unknown, message?: string): asserts value i
 
 /** @deprecated Pass a custom validator to date() instead */
 export function assertIsValidDate(value: unknown, message?: string): asserts value is Date {
+    warn(DEPRECATION_MESSAGE);
+
     if (!(value instanceof Date) || Number.isNaN(value.getTime())) {
         throw new Error(message ?? `Expected ${String(value)} to be a valid date`);
     }
--- src/common/types/validators.ts
+++ src/common/types/validators.ts
@@ -1,37 +1,39 @@
-import {
-    assertIsString,
-    assertIsNumber,
-    assertIsBoolean,
-    assertIsValidDate,
-    assertIsArray,
-} from "./helpers.deprecated.js";
-
 export function stringValidator(value: unknown): string {
-    assertIsString(value);
+    if (typeof value !== "string") {
+        throw new Error(`Expected ${String(value)} to be a string`);
+    }
 
     return value;
 }
 
 export function numberValidator(value: unknown): number {
-    assertIsNumber(value);
+    if (typeof value !== "number") {
+        throw new Error(`Expected ${String(value)} to be a number`);
+    }
 
     return value;
 }
 
 export function booleanValidator(value: unknown): boolean {
-    assertIsBoolean(value);
+    if (typeof value !== "boolean") {
+        throw new Error(`Expected ${String(value)} to be a boolean`);
+    }
 
     return value;
 }
 
 export function dateValidator(value: unknown): Date {
-    assertIsValidDate(value);
+    if (!(value instanceof Date) || Number.isNaN(value.getTime())) {
+        throw new Error(`Expected ${String(value)} to be a valid date`);
+    }
 
     return value;
 }
 
 export function arrayValidator(value: unknown): unknown[] {
-    assertIsArray(value);
+    if (!Array.isArray(value)) {
+        throw new Error(`Expected ${String(value)} to be an array`);
+    }
 
     return value;
 }
