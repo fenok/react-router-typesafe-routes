0f41bccb292282f1e3360ba5c3f956a602a34057
Leonid Fenko <fenok2112@gmail.com>
2023-03-13 23:16:25 +0100

    Add deprecation descriptions

--- src/common/types/type.deprecated.ts
+++ src/common/types/type.deprecated.ts
@@ -1,37 +1,37 @@
-/** @deprecated */
+/** @deprecated Switch to universal types */
 interface Type<TOriginal, TPlain = string, TRetrieved = TOriginal> {
     getPlain: (originalValue: TOriginal) => TPlain;
     getTyped: (plainValue: unknown) => TRetrieved;
     isArray?: boolean;
 }
 
-/** @deprecated */
+/** @deprecated Switch to universal types */
 type ThrowableFallback = { __brand: "throwable" };
 
-/** @deprecated */
+/** @deprecated Switch to universal types */
 interface CallableType<TOriginal, TPlain = string, TRetrieved = TOriginal> extends Type<TOriginal, TPlain, TRetrieved> {
     (fallback: TOriginal | ThrowableFallback): Type<TOriginal, TPlain, TRetrieved> & { __brand: "withFallback" };
 }
 
-/** @deprecated */
+/** @deprecated Switch to universal types */
 type OriginalParams<TTypes> = Params<TTypes, true>;
-/** @deprecated */
+/** @deprecated Switch to universal types */
 type RetrievedParams<TTypes> = Params<TTypes>;
 
-/** @deprecated */
+/** @deprecated Switch to universal types */
 type KeysWithFallback<T> = {
     [TKey in keyof T]: KeyWithFallback<T[TKey], TKey>;
 }[keyof T];
 
-/** @deprecated */
+/** @deprecated Switch to universal types */
 type KeyWithFallback<T, K> = T extends { __brand: "withFallback" } ? K : never;
 
-/** @deprecated */
+/** @deprecated Switch to universal types */
 type Params<TTypes, TUseOriginal extends boolean = false> = {
     [TKey in keyof TTypes]: TypeValue<TTypes[TKey], TUseOriginal>;
 };
 
-/** @deprecated */
+/** @deprecated Switch to universal types */
 type TypeValue<T, TUseOriginal extends boolean> = T extends Type<infer TOriginal, unknown, infer TRetrieved>
     ? TUseOriginal extends true
         ? TOriginal
