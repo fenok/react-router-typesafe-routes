bf9d42baae0bb1ae4672f11309459ff5d0ebb34a
Leonid Fenko <fenok2112@gmail.com>
2023-03-19 16:04:42 +0100

    Rework validators errors

--- src/common/types/validators.ts
+++ src/common/types/validators.ts
@@ -1,14 +1,18 @@
 export function stringValidator(value: unknown): string | undefined {
     if (typeof value !== "undefined" && typeof value !== "string") {
-        throw new Error(`Expected ${String(value)} to be a string`);
+        throw new Error(`${String(value)} is not assignable to 'string | undefined'`);
     }
 
     return value;
 }
 
 export function numberValidator(value: unknown): number | undefined {
-    if (typeof value !== "undefined" && (typeof value !== "number" || Number.isNaN(value))) {
-        throw new Error(`Expected ${String(value)} to be a number`);
+    if (typeof value !== "undefined" && typeof value !== "number") {
+        throw new Error(`${String(value)} is not assignable to 'number | undefined'`);
+    }
+
+    if (Number.isNaN(value)) {
+        throw new Error(`Unexpected NaN`);
     }
 
     return value;
@@ -16,15 +20,19 @@ export function numberValidator(value: unknown): number | undefined {
 
 export function booleanValidator(value: unknown): boolean | undefined {
     if (typeof value !== "undefined" && typeof value !== "boolean") {
-        throw new Error(`Expected ${String(value)} to be a boolean`);
+        throw new Error(`${String(value)} is not assignable to 'boolean | undefined'`);
     }
 
     return value;
 }
 
 export function dateValidator(value: unknown): Date | undefined {
-    if (typeof value !== "undefined" && (!(value instanceof Date) || Number.isNaN(value.getTime()))) {
-        throw new Error(`Expected ${String(value)} to be a Date`);
+    if (typeof value !== "undefined" && !(value instanceof Date)) {
+        throw new Error(`${String(value)} is not assignable to 'Date | undefined'`);
+    }
+
+    if (typeof value !== "undefined" && Number.isNaN(value.getTime())) {
+        throw new Error("Unexpected Invalid Date");
     }
 
     return value;
