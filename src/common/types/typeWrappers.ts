4e13fc60339f19b2c57003bba7c0cf8574383d9c
Leonid Fenko <fenok2112@gmail.com>
2023-03-19 16:05:52 +0100

    Refactor

--- src/common/types/typeWrappers.ts
+++ src/common/types/typeWrappers.ts
@@ -1,5 +1,4 @@
 import { type, UniversalType, Validator } from "./type.js";
-import { stringValidator, numberValidator, booleanValidator, dateValidator } from "./validators.js";
 import { parser } from "./parser.js";
 
 function string(): UniversalType<string | undefined>;
@@ -96,6 +95,46 @@ function union<T extends readonly (string | number | boolean)[]>(value: T | T[nu
             },
         }
     );
+}
+
+function stringValidator(value: unknown): string | undefined {
+    if (typeof value !== "undefined" && typeof value !== "string") {
+        throw new Error(`${String(value)} is not assignable to 'string | undefined'`);
+    }
+
+    return value;
+}
+
+function numberValidator(value: unknown): number | undefined {
+    if (typeof value !== "undefined" && typeof value !== "number") {
+        throw new Error(`${String(value)} is not assignable to 'number | undefined'`);
+    }
+
+    if (Number.isNaN(value)) {
+        throw new Error(`Unexpected NaN`);
+    }
+
+    return value;
+}
+
+function booleanValidator(value: unknown): boolean | undefined {
+    if (typeof value !== "undefined" && typeof value !== "boolean") {
+        throw new Error(`${String(value)} is not assignable to 'boolean | undefined'`);
+    }
+
+    return value;
+}
+
+function dateValidator(value: unknown): Date | undefined {
+    if (typeof value !== "undefined" && !(value instanceof Date)) {
+        throw new Error(`${String(value)} is not assignable to 'Date | undefined'`);
+    }
+
+    if (typeof value !== "undefined" && Number.isNaN(value.getTime())) {
+        throw new Error("Unexpected Invalid Date");
+    }
+
+    return value;
 }
 
 export { string, number, boolean, date, union };
