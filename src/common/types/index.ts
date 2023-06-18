9ca74080f93aa462afbb1706e4112d09a5a74750
Leonid Fenko <fenok2112@gmail.com>
2023-06-18 14:02:25 +0200

    Remove deprecated features

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -2,7 +2,3 @@ export * from "./type.js";
 export * from "./typeWrappers.js";
 export * from "./hashValues.js";
 export * from "./parser.js";
-export * from "./helpers.deprecated.js";
-export * from "./type.deprecated.js";
-export * from "./createType.deprecated.js";
-export * from "./builtInTypes.deprecated.js";
