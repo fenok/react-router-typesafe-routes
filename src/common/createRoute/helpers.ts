d15b18f1340fe83ff0a3f9b640bff5f2d71bc50e
Leonid Fenko <fenok2112@gmail.com>
2023-07-06 00:39:16 +0200

    Move helpers to createRoute

--- src/common/createRoute/helpers.ts
+++
@@ -1,9 +0,0 @@
-export type Merge<T, U> = Readable<Omit<T, keyof U> & U>;
-
-export type Identity<T> = T;
-
-export type Readable<T> = Identity<{
-    [K in keyof T]: T[K];
-}>;
-
-export type ErrorMessage<T extends string> = T & { __brand: ErrorMessage<T> };
