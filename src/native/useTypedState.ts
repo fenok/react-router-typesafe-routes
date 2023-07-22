ce90ea850e018a9f397a500b5e2cd96a09e44286
Leonid Fenko <fenok2112@gmail.com>
2023-07-22 17:18:30 +0200

    Refactor types

--- src/native/useTypedState.ts
+++ src/native/useTypedState.ts
@@ -1,10 +1,10 @@
-import { BaseRoute, OutStateParams, Types } from "../common/index.js";
+import { BaseRoute, OutState, Types } from "../common/index.js";
 import { useLocation } from "react-router-native";
 import { useMemo } from "react";
 
 export function useTypedState<TPath extends string, TTypesMap extends Types>(
     route: BaseRoute<TPath, TTypesMap>
-): OutStateParams<TTypesMap["state"]> {
+): OutState<TTypesMap["state"]> {
     const location = useLocation();
     return useMemo(() => route.$getTypedState(location.state), [route, location.state]);
 }
