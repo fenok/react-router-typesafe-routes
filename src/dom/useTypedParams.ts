13f0e60853c43a6e104922a00e7fc0df261eb39a
Leonid Fenko <fenok2112@gmail.com>
2023-10-07 21:56:26 +0200

    Merge path and types into single param

--- src/dom/useTypedParams.ts
+++ src/dom/useTypedParams.ts
@@ -1,9 +1,9 @@
-import { BaseRoute, Types, OutPathnameParams } from "../common/index.js";
+import { BaseRoute, RouteOptions, OutPathnameParams } from "../common/index.js";
 import { useParams } from "react-router-dom";
 import { useMemo } from "react";
 
-export function useTypedParams<TTypesMap extends Types>(
-  route: BaseRoute<string | undefined, TTypesMap>,
+export function useTypedParams<TTypesMap extends RouteOptions>(
+  route: BaseRoute<TTypesMap>,
 ): OutPathnameParams<TTypesMap["params"]> {
   const params = useParams();
   return useMemo(() => route.$getTypedParams(params), [route, params]);
