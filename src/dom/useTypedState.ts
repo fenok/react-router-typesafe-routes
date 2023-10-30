d7a1a05b0f2295e4cbda77c54ba618498523d3a5
Leonid Fenko <fenok2112@gmail.com>
2023-10-30 19:50:38 +0100

    Ignore irrelevant eslint errors

--- src/dom/useTypedState.ts
+++ src/dom/useTypedState.ts
@@ -4,5 +4,6 @@ import { useMemo } from "react";
 
 export function useTypedState<TOptions extends RouteOptions>(route: BaseRoute<TOptions>): OutState<TOptions> {
   const location = useLocation();
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return useMemo(() => route.$getTypedState(location.state), [route, location.state]);
 }
