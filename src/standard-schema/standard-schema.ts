5a8c2ec7941d0cdd3a272e9935611d602545b4ce
Leonid Fenko <fenok2112@gmail.com>
2025-06-30 20:06:27 +0200

    Add support for Standard Schema

---
+++ src/standard-schema/standard-schema.ts
@@ -0,0 +1,32 @@
+import { type, Type, ParserHint, parser, Parser } from "../lib/index.js";
+import type { StandardSchemaV1 } from "./spec.js";
+
+interface ConfigureOptions {
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  parserFactory: (hint?: ParserHint) => Parser<any, ParserHint>;
+}
+
+function configure({ parserFactory }: ConfigureOptions) {
+  function schema<T>(schema: StandardSchemaV1<unknown, T | undefined>, parser?: Parser<T>): Type<T> {
+    return type(
+      (value: unknown) => {
+        const result = schema["~standard"].validate(value);
+
+        if (result instanceof Promise) throw new Error("Async validation is not supported");
+
+        if (result.issues) {
+          throw new Error(JSON.stringify(result.issues, null, 2));
+        }
+
+        return result.value;
+      },
+      parser ?? parserFactory("unknown"),
+    );
+  }
+
+  return { schema };
+}
+
+const { schema } = configure({ parserFactory: parser });
+
+export { configure, schema };
